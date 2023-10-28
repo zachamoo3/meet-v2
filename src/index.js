0ededf585162338de7b5fe04a19433d66b4f3605
Zachary Moore <zachary.d.moore184@gmail.com>
2023-10-28 12:11:36 -0500

    Setup Atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('a116f27b564d4e7b90b76199f7b16529').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
