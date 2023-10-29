7b6761255ba0dea126ddda94d5dc42a8d4662d89
Zachary Moore <zachary.d.moore184@gmail.com>
2023-10-29 13:48:27 -0500

    Delete InfoAlert.js

--- src/components/InfoAlert.js
+++
@@ -1,9 +0,0 @@
-// src/components/InfoAlert.js
-
-class InfoAlert extends Alert {
-    constructor(props) {
-        super(props);
-        this.color = 'rgb(0, 0, 255)'; // blue
-        this.bgColor = 'rgb(220, 220, 255)'; // light blue
-    };
-};
